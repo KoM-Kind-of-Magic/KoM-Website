27889a01c3d2e46244b28351906dbc283e1f04fe
Matthieu Leproux <matthieu.leproux@gmail.com>
2022-09-02 17:07:13 +0200

    (cp 86749cc) related to #13 added default files for Deck page and Deck Editor page and routes.

---
+++ src/views/DeckEditorView.vue
@@ -0,0 +1,18 @@
+<template>
+  deckeditorview
+</template>
+
+<script>
+
+export default {
+  name: 'DeckEditorView',
+  methods: {
+  },
+  data() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/DeckView.vue
@@ -0,0 +1,18 @@
+<template>
+  deckview
+</template>
+
+<script>
+
+export default {
+  name: 'DeckView',
+  methods: {
+  },
+  data() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
