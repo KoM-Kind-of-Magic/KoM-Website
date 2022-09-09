3d54278d90e3ea9313e65eef2cf84f7589a33209
jenoh <memint.contact@gmail.com>
2022-09-09 10:38:46 +0200

    refacto

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -7,7 +7,25 @@ resource "aws_default_security_group" "default" {
      protocol    = "tcp"
      # Please restrict your ingress to only necessary IPs and ports.
      # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-     cidr_blocks     = ["83.68.192.4/32"]
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+ ingress {
+     # TLS (change to whatever ports you need)
+     from_port   = 80
+     to_port     = 80
+     protocol    = "http"
+     # Please restrict your ingress to only necessary IPs and ports.
+     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+    ingress {
+     # TLS (change to whatever ports you need)
+     from_port   = 80
+     to_port     = 80
+     protocol    = "http"
+     # Please restrict your ingress to only necessary IPs and ports.
+     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+     cidr_blocks     = ["0.0.0.0/0"]
    }
  egress {
      from_port       = 0
