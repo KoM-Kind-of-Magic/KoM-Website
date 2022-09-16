3376b517ab2f677b303e257ec5b6fe94f8b660b3
Matthieu Leproux <matthieu.leproux@gmail.com>
2022-09-16 16:43:36 +0200

    related to #13 added separation by types in deck list sorted alphabetocaly

--- src/views/DeckEditorView.vue
+++ src/views/DeckEditorView.vue
@@ -5,13 +5,16 @@
         Cards in your deck
       </div>
       <div class="cardContainer">
-        <CardLine v-for="card in cards"
-          :key="card.id"
-          :card="card"
-          @removeAllCards="removeAllCards"
-          @removeCard="removeCard"
-          @addCard="addCard"
-        />
+        <div class="cardTypeContainer" v-for="types in Object.keys(sortByTypes)" :key="types">
+          <div class="title">{{types}}</div>
+          <CardLine v-for="card in sortByTypes[types]"
+            :key="card.id"
+            :card="card"
+            @removeAllCards="removeAllCards"
+            @removeCard="removeCard"
+            @addCard="addCard"
+          />
+        </div>
       </div>
     </div>
     <div class="infos-search-container">
@@ -62,6 +65,30 @@ export default {
       console.log(this.cards[index].number);
     },
   },
+  computed: {
+    sortByTypes() {
+      const res = {};
+      const cardTypes = [...new Set(this.cards.map((c) => c.type))];
+      //
+      cardTypes.sort((a, b) => {
+        if (a < b) { return -1; }
+        if (a > b) { return 1; }
+        return 0;
+      });
+      cardTypes.forEach((t) => {
+        res[t] = [];
+      });
+      this.cards.forEach((c) => {
+        res[c.type].push(c);
+      });
+      return res;
+    },
+  },
+  watch: {
+    sortByTypes(newVal) {
+      // console.log(newVal);
+    },
+  },
   components: {
     CardLine,
     TextArea,
@@ -70,30 +97,59 @@ export default {
     return {
       cards: [
         {
+          name: 'Mana vault',
+          id: '6',
+          number: 1,
+          type: 'artifact',
+        },
+        {
           name: 'Yarok',
           id: '1',
           isCommander: true,
           number: 3,
+          type: 'creature',
         },
         {
           name: 'Atraxa',
           id: '2',
           number: 2,
+          type: 'creature',
         },
         {
           name: 'Sliver Queen',
           id: '3',
           number: 1,
+          type: 'creature',
         },
         {
           name: 'Ulamog',
           id: '4',
           number: 25,
+          type: 'creature',
         },
         {
           name: 'Ghired',
           id: '5',
           number: 999,
+          type: 'creature',
+        },
+        {
+          name: 'Mox diamond',
+          id: '7',
+          number: 2,
+          type: 'artifact',
+        },
+        {
+          name: 'Chandra',
+          id: '8',
+          number: 4,
+          type: 'planeswalker',
+        },
+        {
+          name: 'Phyrexian Tower',
+          id: '9',
+          number: 4,
+          type: 'land',
         },
       ],
     };
@@ -128,11 +184,22 @@ export default {
 .currentDeck .title {
   text-align: center;
 }
+.currentDeck .cardTypeContainer .title {
+  font-size: 15px;
+  margin: 10px 0 0 0;
+}
+.currentDeck .cardTypeContainer .title::first-letter {
+  text-transform: capitalize;
+}
+.currentDeck .cardTypeContainer .title::after {
+  height: 0px;
+  background: $dark-background;
+}
 
 .cardContainer {
   overflow-y: scroll;
   padding: 0 0 0 20px;
-  margin: 20px 20px 20px 0;
+  margin: 0 20px 20px 0;
 }
 
 .currentDeck,
